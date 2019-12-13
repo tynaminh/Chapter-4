637f7b1f748438a5fcdbf293591888c92d0625ab
Tyna Nguyen <tn182848@students.davenport.k12.ia.us>
2019-12-13 14:16:42 -0600

    Update files

---
+++ Debugging4.1/Debugging4.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Debugging4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using static System.Console;
+        {
+            static void Main()
+            {
+                const double BASIC_DOG_PRICE = 2.00;
+                const double CHILI_PRICE = 0.69;
+                const double CHEESE_PRICE = 0.49;
+                String wantChili, wantCheese;
+                double price;
+
+                Console.Write("Do you want chili on your dog? ");
+                wantChili = Console.ReadLine();
+                Console.Write("Do you want cheese on your dog? ");
+                wantCheese = Console.ReadLine();
+
+                    if (wantChili = "Y")
+                    {
+                        if (wantCheese = "Y")
+                }
+                        price == BASIC_DOG_PRICE + CHILI_PRICE + CHEESE_PRICE;
+                    else
+                        price == BASIC_DOG_PRICE + CHILI_PRICE;
+                else
+                   if (wantCheese = "Y")
+                    price = BASIC_DOG_PRICE;
+                else
+                    price == BASIC_DOG_PRICE;
+                Console.WriteLine("Your total is {0}", price.ToString("C"));
+            }
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int ai = rnd.Next(1, 3);
+            int userEntry;
+            int wins = 0;
+            bool again = true;
+
+            Console.WriteLine("Welcome to Rock Paper Scissors ");
+            Console.WriteLine("I am your game show host Tron Tricker");
+            Console.WriteLine("######################################");
+            while (again)
+            {
+                Console.WriteLine("Enter the following numbers to play: " +
+                "\n1 - Rock \n2 - Scissors \n3 - Paper");
+                userEntry = Convert.ToInt32(Console.ReadLine());
+
+                if (ai == 1)
+                {
+                    if (userEntry == 1)
+                    {
+                        Console.WriteLine("You tied!");
+                    }
+                    else if (userEntry == 2)
+                    {
+                        Console.WriteLine("You won!!!");
+                        wins = wins + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You lost");
+                    }
+                }
+                else if (ai == 2)
+                {
+                    if (userEntry == 2)
+                    {
+                        Console.WriteLine("You won!");
+                        wins = wins + 1;
+                    }
+                    else if (userEntry == 2)
+                    {
+                        Console.WriteLine("You tied!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You lost");
+                    }
+                }
+                else if (ai == 3)
+                {
+                    if (userEntry == 3)
+                    {
+                        Console.WriteLine("You lost!");
+                    }
+                    else if (userEntry == 2)
+                    {
+                        Console.WriteLine("You won!");
+                        wins = wins + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You tied!");
+                    }
+                }
+                if (wins == 5)
+                {
+                    Console.WriteLine("Congrats you have completed the game!");
+                    again = false;
+                }
+                Console.WriteLine("You have won " + wins + " times");
+            }
+        }
+    }
+}
